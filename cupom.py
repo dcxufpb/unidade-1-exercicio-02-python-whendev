f183701b3ed5c5bd053009cbf978055c15e9921a
Whendel Morais <whendel.morais.9@gmail.com>
2020-09-16 03:20:58 +0000

    exercise solved

--- cupom.py
+++ cupom.py
@@ -1,4 +1,4 @@
-nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
+nome_loja = "Arco Dourados Com. de Alimentos LTDA"
 logradouro = "Av. Projetada Leste"
 numero = 500
 complemento = "EUC F32/33/34"
@@ -12,5 +12,5 @@ cnpj = "42.591.651/0797-34"
 inscricao_estadual = "244.898.500.113"
 
 def imprime_dados_loja():
-    # Implemente aqui
-    return "" 
+  show = "%s\n%s, %d %s\n%s - %s - %s\nCEP:%s Tel %s\n%s\nCNPJ: %s\nIE: %s\n" %(nome_loja,logradouro,numero,complemento,bairro,municipio,estado,cep,telefone,observacao,cnpj,inscricao_estadual)
+  return show
--- cupom_test.py
+++ cupom_test.py
@@ -1,17 +1,17 @@
 import cupom;
 
-nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
-logradouro = "Av. Projetada Leste"
-numero = 500
-complemento = "EUC F32/33/34"
-bairro = "Br. Sta Genebra"
-municipio = "Campinas"
-estado = "SP"
-cep = "13080-395"
-telefone = "(19) 3756-7408"
-observacao = "Loja 1317 (PDP)"
-cnpj = "42.591.651/0797-34"
-inscricao_estadual = "244.898.500.113"
+cupom.nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
+cupom.logradouro = "Av. Projetada Leste"
+cupom.numero = 500
+cupom.complemento = "EUC F32/33/34"
+cupom.bairro = "Br. Sta Genebra"
+cupom.municipio = "Campinas"
+cupom.estado = "SP"
+cupom.cep = "13080-395"
+cupom.telefone = "(19) 3756-7408"
+cupom.observacao = "Loja 1317 (PDP)"
+cupom.cnpj = "42.591.651/0797-34"
+cupom.inscricao_estadual = "244.898.500.113"
 
 def test_exercicio1():
     assert cupom.imprime_dados_loja() == '''Arcos Dourados Com. de Alimentos LTDA
@@ -37,18 +37,18 @@ def test_exercicio2_tudo_vazio():
     global cnpj
     global inscricao_estadual
     
-    nome_loja = ""
-    logradouro = ""
-    numero = 0
-    complemento = ""
-    bairro = ""
-    municipio = ""
-    estado = ""
-    cep = ""
-    telefone = ""
-    observacao = ""
-    cnpj = ""
-    inscricao_estadual = ""
+    cupom.nome_loja = ""
+    cupom.logradouro = ""
+    cupom.numero = 0
+    cupom.complemento = ""
+    cupom.bairro = ""
+    cupom.municipio = ""
+    cupom.estado = ""
+    cupom.cep = ""
+    cupom.telefone = ""
+    cupom.observacao = ""
+    cupom.cnpj = ""
+    cupom.inscricao_estadual = ""
 
     assert cupom.imprime_dados_loja() == '''
 , 0 
@@ -74,19 +74,25 @@ def test_exercicio2_customizado():
     global inscricao_estadual
     
     # Defina seus próprios valores para as variáveis a seguir
-    nome_loja = ""
-    logradouro = ""
-    numero = 0
-    complemento = ""
-    bairro = ""
-    municipio = ""
-    estado = ""
-    cep = ""
-    telefone = ""
-    observacao = ""
-    cnpj = ""
-    inscricao_estadual = ""
+    cupom.nome_loja = "LOJAS AMERICANAS S.A."
+    cupom.logradouro = "R SACADURA CABRAL"
+    cupom.numero = 102
+    cupom.complemento = ""
+    cupom.bairro = "GAMBOA"
+    cupom.municipio = "RIO DE JANEIRO"
+    cupom.estado = "RJ"
+    cupom.cep = "20.221-160"
+    cupom.telefone = "(21) 2206-6708"
+    cupom.observacao = "47.11-3-02 Comercio varejista de mercadorias em geral"
+    cupom.cnpj = "33.014.556/0001-96"
+    cupom.inscricao_estadual = "85.687.08-5"
 
     #E atualize o texto esperado abaixo
-    assert cupom.imprime_dados_loja() == '''
+    assert cupom.imprime_dados_loja() == '''LOJAS AMERICANAS S.A.
+R SACADURA CABRAL, 102 
+GAMBOA - RIO DE JANEIRO - RJ
+CEP:20.221-160 Tel (21) 2206-6708
+47.11-3-02 Comercio varejista de mercadorias em geral
+CNPJ: 33.014.556/0001-96
+IE: 85.687.08-5
 '''
